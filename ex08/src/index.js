26dae290520c0978f492ee950f2083e1a12ad012
jpjanic <jasmin.pjanic@yahoo.com>
2021-03-25 02:41:00 +0100

    Added index file

---
+++ ex08/src/index.js
@@ -0,0 +1,52 @@
+// Lodash
+const _ = require('lodash');
+
+// Only change code below this line
+// users nested array with four objects starts here
+let users = [
+  { firstName: 'John', lastName: 'Doe', age: 24, gender: 'male' },
+  { firstName: 'Jane', lastName: 'Doe', age: 5, gender: 'female' },
+  { firstName: 'Jim', lastName: 'Carrey', age: 54, gender: 'male' },
+  { firstName: 'Kate', lastName: 'Winslet', age: 40, gender: 'female' },
+];
+// users nested array with four objects ends here
+
+
+// getUsers function - list of users starts here
+function getUsers() {
+      var output = "";
+      for (var i = 0; i < users.length; i++) {
+        output += `${users[i].firstName} ${users[i].lastName} is ${users[i].age}, ${users[i].gender}\n`;
+      }
+      return output;
+    }
+
+// getUsers function - list of users ends here
+
+
+// findUser (lastName, gender) function starts here
+
+
+function findUser(lastName, gender) {
+  try {
+    // add appropriate code here
+    var user = _.find(users, (obj) => {
+			if (obj.lastName === lastName && obj.gender === gender) {
+				return true;
+			}
+		});
+		var iFindUser = `${user.firstName} ${user.lastName} is ${user.age}, ${user.gender}`;
+		return iFindUser;
+   
+  } catch (error) {
+    return error; // Change this line
+    // Change this line
+  }
+}
+
+// findUser(lastName, gender) function ends here
+// Only change code above this line
+getUsers();
+findUser("Doe", "male"); // Change this line
+
+module.exports = findUser;
---
+++ ex09/src/index.js
@@ -0,0 +1,46 @@
+// Lodash
+const _ = require('lodash');
+
+// Only change code below this line
+// users nested array with four objects starts here
+var users = [
+  { id: 1, firstName: 'John', lastName: 'Doe', age: 24, gender: 'male' },
+  { id: 2, firstName: 'Jane', lastName: 'Doe', age: 5, gender: 'female' },
+  { id: 3, firstName: 'Jim', lastName: 'Carrey', age: 54, gender: 'male' },
+  { id: 4, firstName: 'Kate', lastName: 'Winslet', age: 40, gender: 'female' },
+];
+// users nested array with four objects ends here
+
+
+// getUsers function - list of users starts here
+function getUsers() {
+	var output = "";
+	for (var i = 0; i < users.length; i++) {
+		output += `${users[i].id} - ${users[i].firstName} ${users[i].lastName} is ${users[i].age}, ${users[i].gender}\n`;
+	}
+	return output;
+}
+// getUsers function - list of users ends here
+
+
+// findUser (id) function starts here
+function findUserById(id) {
+	try {
+    // add appropriate code here
+		var user = _.find(users, (obj) => {
+			if (obj.id === id) {
+				return true;
+			}
+		});
+		var iFindUser = `${user.id} - ${user.firstName} ${user.lastName} is ${user.age}, ${user.gender}`;
+		return iFindUser;
+	} catch (error) {
+		return error; // Change this line
+	}
+}
+// findUserById(id) function ends here
+// Only change code above this line
+getUsers();
+findUserById(); // Change this line
+
+module.exports = findUserById;
